3c91c54acfacaf134274b5364e06f7414c6c032e
John Doe <uncolourfulf116@gmail.com>
2019-08-26 10:52:16 +0300

    intro done adapt

--- js/script.js
+++ js/script.js
@@ -1,6 +1,1 @@
-jQuery(document).ready(function() {
-	$(".intro-wrap .video-button").click(function(){
-		$(this).hide();
-		$(".intro-video").find("iframe").contents().find(".video").get(0).play();
-	});
-});
+
